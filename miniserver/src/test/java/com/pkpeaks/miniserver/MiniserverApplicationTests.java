d982ce4982eeaca1b7b7548e0e538a2a87481c90
PRADEESH KUMAR S <pradeeshkumars@srmasc.ac.in>
2026-08-01 20:48:54 +0530

    Update MiniserverApplicationTests.java

--- miniserver/src/test/java/com/pkpeaks/miniserver/MiniserverApplicationTests.java
+++ miniserver/src/test/java/com/pkpeaks/miniserver/MiniserverApplicationTests.java
@@ -1,13 +1,31 @@
 package com.pkpeaks.miniserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class MiniserverApplicationTests {
+import java.util.Map;
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootApplication
+@RestController
+public class MiniserverApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(MiniserverApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of(
+                "message", "Hello, World!"
+        );
+    }
+
+    @GetMapping("/status")
+    public Map<String, String> status() {
+        return Map.of(
+                "status", "Server is running"
+        );
+    }
 }
